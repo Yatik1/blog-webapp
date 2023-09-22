32d72ecdff492ae6c8930bb57d241e27ac5edb05
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-09-22 13:20:03 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './App.css'
-
+import Header from './components/Header';
+import './App.css';
+import Post from './components/Post';
+import {Routes, Route} from 'react-router-dom'
+import Login from './components/Login';
+import Register from './components/Register';
 function App() {
   return (
-    <div>
-       <h1>Hello </h1>
-    </div>
-  )
+    <> <Header />
+    <Routes>
+      <Route path='/' index element ={
+          <>
+           <Post />
+          </>
+      } />
+      
+      <Route path={'/login'} element={
+         <Login />
+      } />
+
+      <Route path={'/register'} element={
+            <Register />
+         } />
+
+    </Routes></>
+  );
 }
 
-export default App
+export default App;
