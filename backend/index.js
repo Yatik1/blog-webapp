c4f27ab0d3bf37b170b31a34561734273514be4a
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-09-26 18:34:50 +0530

    ::encryption handling

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const User = require('./models/User')
+const bcrypt = require("bcrypt");
+const User = require('./models/User');
 
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yatiksrivastava1:5oiJwFGY43FHHKQw@cluster0.1fw5w0f.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://yatiksrivastava1:Dfva4UQdwbsiJ9Y9@cluster0.1fw5w0f.mongodb.net/?retryWrites=true&w=majority')
 
 app.get('/register' , (req,res) => {
     res.json('test ok2');
@@ -18,9 +20,13 @@ app.get('/register' , (req,res) => {
 app.post('/register' , async (req,res) => {
     const {username,password} = req.body;
     try {
-        const UserDoc = await User.create({username,password});
+        const UserDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt),
+        });
         res.json(UserDoc);
-    } catch (e) {
+    } catch(e) {
+        console.log(e)
         res.status(400).json(e)
     }
 });
