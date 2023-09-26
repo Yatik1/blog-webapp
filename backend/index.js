7759c81899d744c0ff70fa369b5642b5c29fe934
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-09-26 17:54:08 +0530

    ::database connection

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+
+const User = require('./models/User')
+
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 
+mongoose.connect('mongodb+srv://yatiksrivastava1:5oiJwFGY43FHHKQw@cluster0.1fw5w0f.mongodb.net/?retryWrites=true&w=majority')
+
 app.get('/register' , (req,res) => {
     res.json('test ok2');
 });
 
-app.post('/register' , (req,res) => {
+app.post('/register' , async (req,res) => {
     const {username,password} = req.body;
-    res.json({requestData: {username,password}});
+    try {
+        const UserDoc = await User.create({username,password});
+        res.json(UserDoc);
+    } catch (e) {
+        res.status(400).json(e)
+    }
 });
 
 app.listen(4000)
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -11,11 +11,18 @@ function Register() {
 
    async function register(ev){
        ev.preventDefault();
-       await fetch('http://localhost:4000/register' , {
-           method: 'POST',
+
+       const response = await fetch('http://localhost:4000/register' , {
+           method:'POST',
            body: JSON.stringify({username ,password}),
            headers: {'Content-Type': 'application/json'},
        })
+
+       if(response.status === 200){
+        alert('Registration Successful !')
+       } else {
+         alert('Registration Failed !! ')
+       }
    }
 
   return (
