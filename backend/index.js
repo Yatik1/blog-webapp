ae0b0a7000bd440a5effe3640c4ef3ad73bf3193
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-09-27 18:04:25 +0530

    ::implementing login and logout validation

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,15 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const User = require('./models/User');
 const jwt = require('jsonwebtoken')
-
+const cookieParser = require('cookie-parser')
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
 const secret= 'asehbfufvihncugbcb78bicgn8bcr797cby';
 
 app.use(cors({credentials: true , origin: 'http://localhost:5173'}));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://yatiksrivastava1:Dfva4UQdwbsiJ9Y9@cluster0.1fw5w0f.mongodb.net/?retryWrites=true&w=majority')
 
@@ -33,20 +34,34 @@ app.post('/register' , async (req,res) => {
     }
 });
 
-app.post('/login' , async (req,res) => {
+app.post('/login', async (req,res) => {
     const {username,password} = req.body;
     const userDoc = await User.findOne({username});
-    const passOk = bcrypt.compareSync(password , userDoc.password);
-    // res.json(passOk)
+    const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
-        //login
-        jwt.sign({username , id: userDoc._id}, secret , {} , (err,token) => {
-            if (err) throw err; 
-            res.cookie('token' , token).json('ok');
-        })
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
     } else {
-        res.status(400).json('wrong crendentials')
+      res.status(400).json('wrong credentials');
     }
+  });
+
+app.get("/profile", (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token , secret, {} , (err,info) => {
+        if (err) throw err;
+        res.json(info); 
+    })
+})
+
+app.post('/logout' , (req,res) => {
+    res.cookie('token' , '').json('ok');
 })
 
 app.listen(4000)
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css'
 import { Link } from 'react-router-dom'
 
 function Header() {
+
+  const [username , setUsername] = useState(null)
+
+  useEffect(() => {
+     fetch('http://localhost:4000/profile' , {
+       credentials: 'include',
+     }).then( response => {
+         response.json().then(userInfo => {
+           setUsername(userInfo.username);
+         })
+     })
+  },[])
+
+  function logout() {
+    fetch('http://localhost:4000/logout' , {
+      credentials:'include' , 
+      method:'POST',
+    })
+    setUsername(null);
+  }
+
   return (
    <main>
      <header>
         <Link to="/" className='logo'>MyBlog</Link>
          <nav>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
+           {username && (
+            <>
+              <Link to='/create'>Create new post</Link>
+              <a onClick={logout}>Logout</a>
+            </>
+           )}
+           {!username && (
+            <>
+                 <Link to="/login">Login</Link>
+                 <Link to="/register">Register</Link>
+            </>
+           )}
+
          </nav>
      </header>
    </main>
