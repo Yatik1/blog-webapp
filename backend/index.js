d58992b9f45a203e131491dff6d9ad7638fccf0f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-01 16:32:58 +0530

    ::Post Database connection and creation

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const User = require('./models/User');
+const Post = require('./models/Post')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const multer = require('multer')
+const uploadMiddleware = multer({dest : 'uploads/'})
+const fs = require('fs')
+
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
@@ -62,6 +67,26 @@ app.get("/profile", (req,res) => {
 
 app.post('/logout' , (req,res) => {
     res.cookie('token' , '').json('ok');
+})
+
+app.post('/post' , uploadMiddleware.single('file') , async (req,res) => {
+  
+  const {originalname , path} = req.file;
+  const parts = originalname.split('.')
+  const ext = parts[parts.length -1]
+  const newPath = path + '.' + ext; 
+
+  fs.renameSync(path , newPath)
+  
+  const {title,summary,content} = req.body;
+  const postDoc = await Post.create({
+    title , 
+    summary,
+    content,
+    cover : newPath,
+  })
+
+  res.json(postDoc)
 })
 
 app.listen(4000)
