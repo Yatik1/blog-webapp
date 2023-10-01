448d454720980dada5753ca2a19cabc48e499af1
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-02 00:27:18 +0530

    ::posts from database

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "../App.css"
-function Post() {
+
+import {formatISO9075 } from 'date-fns';
+
+function Post({_id,title,summary,cover,content,createdAt,author}) {
   return (
     <main>
          <div className='post'>
@@ -8,12 +11,12 @@ function Post() {
                 <img src="https://i.pinimg.com/564x/b9/52/bb/b952bb9bb9d4048af66ee2d2321bf6e3.jpg" alt="" />
             </div>
             <div className='texts'>
-                <h2>How to Write Vivid Descriptions</h2>
+                <h2>{title}</h2>
                 <p className='info'>
                     <a className='author'>Yatik Srivastava</a>
-                    <time>2023-04-06 12:00</time>
+                    <time>{formatISO9075(new Date(createdAt))}</time>
                 </p>
-                <p className='summary'>Author Eli K.P. William offers advice on how to write vivid descriptions while still being mindful of pacing and plot in a novel by using examples of the balance between the two.</p>
+                <p className='summary'>{summary}</p>
             </div>
          </div>
     </main>
