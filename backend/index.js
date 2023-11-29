e0f26de62f423aac7124d23ef483ac09acb0f263
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-29 23:31:49 +0530

    middleware

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,10 @@ const salt = bcrypt.genSaltSync(10);
 const secret= 'asehbfufvihncugbcb78bicgn8bcr797cby';
 
 app.use(cors({credentials: true , origin: 'http://localhost:5173'}));
+
 app.use(express.json());
 app.use(cookieParser());
+
 app.use('/uploads' , express.static(__dirname + '/uploads'))
 
 mongoose.connect(process.env.URI);
